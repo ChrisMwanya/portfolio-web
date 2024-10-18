b88c0630f916680095cb50b1e19ba8d165faecf7
Chris Mwanya <cmwanya@gmail.com>
2024-10-18 11:31:25 +0100

    fix: error

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -5,21 +5,6 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Chris Mwanya',
   description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
-  metadataBase: new URL(`${process.env.VERCEL_URL}`),
-  openGraph: {
-    images: ['/assets/images/fordT.webp'],
-    title: 'Chris Mwanya',
-    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
-    url: `${process.env.VERCEL_URL}`,
-    siteName: 'Chris Mwanya',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Chris Mwanya',
-    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
-    images: '/assets/images/fordT.webp',
-  },
 };
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return <div className="mt-5 text-secondary-foreground">{children}</div>;
