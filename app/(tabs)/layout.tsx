2fdf1aad0d1938166e9ab28e23ac4c3983d61518
Chris Mwanya <cmwanya@gmail.com>
2024-10-18 11:25:59 +0100

    Update environment variables and add metadata in layout components

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,5 +1,26 @@
 import React, { PropsWithChildren } from 'react';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Chris Mwanya',
+  description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+  metadataBase: new URL(`${process.env.VERCEL_URL}`),
+  openGraph: {
+    images: ['/assets/images/fordT.webp'],
+    title: 'Chris Mwanya',
+    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+    url: `${process.env.VERCEL_URL}`,
+    siteName: 'Chris Mwanya',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Chris Mwanya',
+    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+    images: '/assets/images/fordT.webp',
+  },
+};
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return <div className="mt-5 text-secondary-foreground">{children}</div>;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 import { Home } from '@/components/home/home';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Chris Mwanya',
+  description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+  metadataBase: new URL(`${process.env.VERCEL_URL}`),
+  openGraph: {
+    images: ['/assets/images/fordT.webp'],
+    title: 'Chris Mwanya',
+    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+    url: `${process.env.VERCEL_URL}`,
+    siteName: 'Chris Mwanya',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Chris Mwanya',
+    description: 'Fullstack Developer. Trainer. Human. Love sharing knowledge',
+    images: '/assets/images/fordT.webp',
+  },
+};
 const Index = () => <Home />;
 
 export default Index;
